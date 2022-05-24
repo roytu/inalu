Fixed gradient bugs

nalu_layers.py
@@ -9,13 +9,15 @@ class Nalui2Layer(tf.keras.layers.Layer):
         self.num_outputs = num_outputs
 
     def build(self, input_shape):
-        w_hat1 = self.add_weight(
+        self.w_hat1 = self.add_weight(
                 "w_hat1",
                 shape=(input_shape[-1], self.num_outputs),
-                initializer="random_normal",
+                initializer=tf.keras.initializers.RandomNormal(
+                    mean=0.88, stddev=0.2, seed=None
+                    ),
                 trainable=True)
 
-        m_hat1 = self.add_weight(
+        self.m_hat1 = self.add_weight(
                 "m_hat1",
                 shape=(input_shape[-1], self.num_outputs),
                 initializer=tf.keras.initializers.RandomNormal(
@@ -23,7 +25,7 @@ class Nalui2Layer(tf.keras.layers.Layer):
                     ),
                 trainable=True)
 
-        w_hat2 = self.add_weight(
+        self.w_hat2 = self.add_weight(
                 "w_hat2",
                 shape=(input_shape[-1], self.num_outputs),
                 initializer=tf.keras.initializers.RandomNormal(
@@ -31,7 +33,7 @@ class Nalui2Layer(tf.keras.layers.Layer):
                     ),
                 trainable=True)
 
-        m_hat2 = self.add_weight(
+        self.m_hat2 = self.add_weight(
                 "m_hat2",
                 shape=(input_shape[-1], self.num_outputs),
                 initializer=tf.keras.initializers.RandomNormal(
@@ -39,7 +41,7 @@ class Nalui2Layer(tf.keras.layers.Layer):
                     ),
                 trainable=True)
 
-        G1 = self.add_weight(
+        self.G1 = self.add_weight(
                 "g",
                 shape=(self.num_outputs,),
                 initializer=tf.keras.initializers.RandomNormal(
@@ -47,26 +49,26 @@ class Nalui2Layer(tf.keras.layers.Layer):
                     ),
                 trainable=True)
 
-        self.W1 = tf.tanh(w_hat1) * tf.sigmoid(m_hat1)
-        self.W2 = tf.tanh(w_hat2) * tf.sigmoid(m_hat2)
-        self.g1 = tf.sigmoid(G1)
+    def call(self, inputs, training=None):
+        W1 = tf.math.tanh(self.w_hat1) * tf.math.sigmoid(self.m_hat1)
+        W2 = tf.tanh(self.w_hat2) * tf.sigmoid(self.m_hat2)
+        g1 = tf.sigmoid(self.G1)
 
-    def call(self, inputs):
         # sign
-        W1s = tf.reshape(self.W2, [-1])  # flatten W1s to (200)
+        W1s = tf.reshape(W2, [-1])  # flatten W1s to (200)
         W1s = tf.abs(W1s)
-        Xs = tf.concat([inputs] * self.W1.shape[1], axis=1)
-        Xs = tf.reshape(Xs, shape=[-1, self.W1.shape[0] * self.W1.shape[1]])
+        Xs = tf.concat([inputs] * W1.shape[1], axis=1)
+        Xs = tf.reshape(Xs, shape=[-1, W1.shape[0] * W1.shape[1]])
         sgn = tf.sign(Xs) * W1s + (1 - W1s)
-        sgn = tf.reshape(sgn, shape=[-1, self.W1.shape[1], self.W1.shape[0]])
+        sgn = tf.reshape(sgn, shape=[-1, W1.shape[1], W1.shape[0]])
         ms1 = tf.reduce_prod(sgn, axis=2)
-        a1 = tf.matmul(inputs, self.W1)
+        a1 = tf.matmul(inputs, W1)
 
-        m1 = tf.exp(tf.minimum(tf.matmul(tf.math.log(tf.maximum(tf.abs(inputs), 1e-7)), self.W2), 20))  # clipping
+        m1 = tf.exp(tf.minimum(tf.matmul(tf.math.log(tf.maximum(tf.abs(inputs), 1e-7)), W2), 20))  # clipping
         
-        out_add = self.g1 * a1
+        out_add = g1 * a1
         out_sgn = tf.clip_by_value(ms1, -1, 1)
-        out_mul = (1 - self.g1) * m1
+        out_mul = (1 - g1) * m1
         out = out_add + out_mul * out_sgn
         return out
 